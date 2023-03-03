0868edee80c8d07f05126167e4f7a3a2f352cadc
FokiDoki <23121394+FokiDoki@users.noreply.github.com>
2023-03-03 16:32:12 +0300

    Initial commit

---
+++ src/main/java/com/snakestore/SnakeStoreApplication.java
@@ -0,0 +1,13 @@
+package com.snakestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakeStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnakeStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/snakestore/SnakeStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snakestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakeStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
